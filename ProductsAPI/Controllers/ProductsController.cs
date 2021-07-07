16c49689790dbfd54be95bc3392ebcad2f3cae2f
leandrogabatel <legabatel@hotmail.com>
2021-07-07 14:33:04 -0300

    Initial Commit

---
+++ ProductsAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("get/{id:int}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public IActionResult Get(int id)
+        {
+            if(id > 0)
+                return Ok(new {
+                    Id = id,
+                    Descricao = $"Produto: {id}"
+                });
+
+            return NotFound();
+        }
+    }
+}
